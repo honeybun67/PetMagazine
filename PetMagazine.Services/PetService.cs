a45f8b5c648950cb9e3efe5ab9aab71c229f1622
aylin <ailin.perushinska@gmail.com>
2024-03-14 13:48:12 +0200

    Update service

--- PetMagazine.Services/PetService.cs
+++ PetMagazine.Services/PetService.cs
@@ -1,12 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace PetMagazine.Services
+﻿namespace PetMagazine.Services
 {
-    internal class PetService
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    public class PetService
     {
     }
 }
